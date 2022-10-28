cf9ae0a58921c7de802633177ac835a70b624a5f
Bhavya-Parikh <bhavyaparikh0@gmail.com>
2022-10-28 15:22:47 +0530

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const mongoose = require('mongoose')
